d419d663f36a69da297a9afda47e9eb583823153
Da Huang <dhuang.cn@gmail.com>
2016-03-12 14:36:48 +0800

    Added Tag.
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

---
+++ document/tag.h
@@ -0,0 +1,14 @@
+#ifndef DOCUMENT_TAG_H_
+#define DOCUMENT_TAG_H_
+
+#include "document/vocabulary.h"
+
+namespace deeplearning {
+namespace embedding {
+
+using Tag = Vocabulary::Item;
+
+}  // namespace embedding
+}  // namespace deeplearning
+
+#endif  // DOCUMENT_TAG_H_
