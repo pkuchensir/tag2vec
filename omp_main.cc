94639a01c5bd2f0f51d7b3ae76be87729005bb0f
Da Huang <dhuang.cn@gmail.com>
2016-03-12 12:37:56 +0800

    Tested omp.
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

---
+++ omp_main.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <omp.h>
+#include <unistd.h>
+
+static int i = 0;
+
+void F(int p) {
+  int k;
+  #pragma omp critical(A)
+  {
+    k = i + 1;
+    sleep(1);
+    i = k;
+    std::cerr << "f(" << p << "): " << i << std::endl;
+  }
+}
+
+void G(int p) {
+  int k;
+  #pragma omp critical(A)
+  {
+    k = i + 1;
+    sleep(1);
+    i = k;
+    std::cerr << "g(" << p << "): " << i << std::endl;
+  }
+}
+
+int main(int argc, char** argv) {
+  omp_set_num_threads(4);
+  #pragma omp parallel for
+  for (int i = 0; i < 10; ++i) {
+    if (i & 1) F(i);
+    else G(i);
+  }
+  return 0;
+}
