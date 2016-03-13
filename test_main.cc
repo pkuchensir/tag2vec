916aa61af7b5ab54272611cc4df1164e679f832b
Da Huang <dhuang.cn@gmail.com>
2016-03-13 11:33:15 +0800

    Better test_main.
    Signed-off-by: Da Huang <dhuang.cn@gmail.com>

--- test_main.cc
+++ test_main.cc
@@ -1,18 +1,36 @@
 #include <eigen3/Eigen/Core>
 #include <fstream>
 #include <string>
+#include <vector>
 
-// #include "document/standard_document_iterator.h"
 #include "model/tag2vec.h"
+#include "document/score_item.h"
+#include "util/logging.h"
 
 namespace deeplearning {
 namespace embedding {
 namespace {
 
+void PrintScoreItemVec(const std::vector<ScoreItem>& score_item_vec) {
+  for (const ScoreItem& score_item : score_item_vec) {
+    LOG(INFO) << score_item.ToString();
+  }
+}
+
 void Test(const std::string& model_path) {
   Tag2Vec tag2vec;
   std::ifstream fin(model_path);
   Tag2Vec::Read(&fin, &tag2vec);
+
+  std::string tag = "music";
+  LOG(INFO) << tag << ":";
+  std::vector<ScoreItem> ans = tag2vec.MostSimilar(tag2vec.TagVec("music"), 20);
+  PrintScoreItemVec(ans);
+
+  tag = "food";
+  LOG(INFO) << tag << ":";
+  ans = tag2vec.MostSimilar(tag2vec.TagVec("music"), 20);
+  PrintScoreItemVec(ans);
 }
 
 }  // namespace
@@ -20,7 +38,6 @@ void Test(const std::string& model_path) {
 }  // namespace deeplearning
 
 int main(int argc, char** argv) {
-  // Eigen::initParallel();
   deeplearning::embedding::Test("/home/dhuang/tag2vec.model");
   return 0;
 }
